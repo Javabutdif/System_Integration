0f4cee28531c92a99799f60af0c73fe9566d485d
unknown <TT128@ccs.local>
2024-03-12 02:07:42 +0800

    login page change

--- static/Login.php
+++ static/Login.php
@@ -9,42 +9,95 @@
 
 <link rel="stylesheet" href="/css/style.css">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	<style>
+		.divider:after,
+.divider:before {
+content: "";
+flex: 1;
+height: 1px;
+background: #eee;
+}
+.h-custom {
+height: calc(100% - 73px);
+}
+@media (max-width: 450px) {
+.h-custom {
+height: 100%;
+}
+}
+		</style>
 	</head>
 	
 	<body>
-	
-
 	<h1 style="color: black; text-align:center"> 
-		College of Computer Studies
+		College of Computer Studies Sit-In Monitoring System
 	</h1>
-	
-	<div class="text-center" >
-		
-		<form action="Login.php" method="GET" class="form-group container d-block" style="padding-bottom: 30%; padding-top:10%">
-			<img src="/jems/images/uc-logo.jpg" class="rounded-circle col-2">
-			<img src="/jems/images/ccs logo.jpg" class="rounded-circle col-2">
-			<h1>Login</h1>
-			</br>
-			<div>
+	<section class="vh-100" >
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="/jems/images/uc-logo.jpg"
+          class="img-fluid" alt="CCS Logo">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
 
-			<input class="border-0 shadow w-50 " placeholder="Id Number" type="text" name="idNum" required/>
-			
-			</div>
-			</br>
-			</br>
-			<input class="border-0 shadow w-50 " placeholder="Password" type="password" name="password" required/>
-			
-			</br>
-			</br>
-			<button type="submit" name="submit" class="btn btn-outline-primary ">Login</button>
-			</br>
-		</br>
-			<p class="text-black">
-				No Account? Click <a style="color: dodgerblue;" href="Register.php">here!</a>
-			</p>
-			
-		</form>
-		</div>
+        <form action="Login.php" method="GET">
+        
+
+          
+
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <input type="text" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Enter a valid id number" name="idNum" required />
+            <label class="form-label" for="form3Example3">ID Number</label>
+          </div>
+
+          <!-- Password input -->
+          <div class="form-outline mb-3">
+            <input type="password" id="form3Example4" class="form-control form-control-lg"
+              placeholder="Enter password" name="password" required />
+            <label class="form-label" for="form3Example4">Password</label>
+          </div>
+
+          <div class="d-flex justify-content-between align-items-center">
+            <!-- Checkbox -->
+            <div class="form-check mb-0">
+              <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+              <label class="form-check-label" for="form2Example3">
+                Remember me
+              </label>
+            </div>
+            <a href="#!" class="text-body">Forgot password?</a>
+          </div>
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <button type="submit" class="btn btn-primary btn-lg" name="submit"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="Register.php"
+                class="link-danger">Register</a></p>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+  <div
+    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+    <!-- Copyright -->
+    <div class="text-white mb-3 mb-md-0">
+      Copyright © 2020. All rights reserved.
+    </div>
+    <!-- Copyright -->
+
+    <!-- Right -->
+  
+</section>
+	
+
+	
+	
+	
 
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
