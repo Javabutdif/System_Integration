f1502bb5e0e42b15146de48939bc1af209001422
Anton James Genabio <jamesgenabio31@gmail.com>
2024-03-17 15:01:57 +0800

    fix?

--- static/Admin.php
+++ static/Admin.php
@@ -1,9 +1,10 @@
+
 <?php
   session_start();
   if($_SESSION["admin_id_number"] == 0  ){
     header("Location: Login.php");
 		exit();
-	}
+	} 
  
 
   if (isset($_GET["search"])) {
@@ -59,7 +60,7 @@
       $displayModal = true;
   } else {
     
- 
+   
       // No record found
       echo '<script>Swal.fire({
           icon: "error",
@@ -81,6 +82,8 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.bootstrap5.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
@@ -280,9 +283,15 @@
 
 
 
+
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.bootstrap5.js"></script>
 <script>
   <?php
     if($_SESSION["admin_id"] === 1){
@@ -294,6 +303,8 @@
       $_SESSION["admin_id"] = 0;
     }
   ?>
+  new DataTable('#example');
+
 </script>
 </body>
 </html>
@@ -301,18 +312,16 @@
 
 
 
-<script>
-        // Check if PHP variable $displayModal is true, then show the modal
-        <?php if ($displayModal): ?>
+
+        <script>
+  <?php if ($displayModal): ?>
             $(document).ready(function(){
                 $('#exampleModalCenter').modal('show');
             });
         <?php
           
       endif; ?>
-
-
-      
-    </script>
+  </script>
+   
 
 
