fb587b58e16b6e071a270eefbbdb3b9d382b17a4
unknown <TT128@ccs.local>
2024-03-19 03:34:27 +0800

    sql export added

---
+++ ccs_system.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2024 at 08:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ccs_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id_number` int(11) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `middleName` varchar(50) NOT NULL,
+  `yearLevel` int(11) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `course` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id_number`, `lastName`, `firstName`, `middleName`, `yearLevel`, `password`, `course`, `email`, `address`, `role`) VALUES
+(200, 'Sandalo', 'Jude Jefferson', 'S.', 3, '123', 'BSIT', 'judejefferson@gmail.com', 'Lorega', ''),
+(19835644, 'Genabio', 'Anton James', 'J', 3, '123', 'BSIT', 'jamesgenabio31@gmail.com', 'San Isidro', ''),
+(31313123, 'Nipaya', 'Dione Louis', 'Louis', 3, '123', 'BSCS', 'dionelouis@gmail.com', 'balamban', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_session`
+--
+
+CREATE TABLE `student_session` (
+  `id_number` int(11) NOT NULL,
+  `session` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_session`
+--
+
+INSERT INTO `student_session` (`id_number`, `session`) VALUES
+(200, 30),
+(19835644, 30),
+(31313123, 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_sit_in`
+--
+
+CREATE TABLE `student_sit_in` (
+  `sit_id` int(11) NOT NULL,
+  `id_number` int(11) NOT NULL,
+  `sit_purpose` varchar(50) NOT NULL,
+  `sit_lab` varchar(20) NOT NULL,
+  `sit_login` date NOT NULL,
+  `sit_logout` date NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_sit_in`
+--
+
+INSERT INTO `student_sit_in` (`sit_id`, `id_number`, `sit_purpose`, `sit_lab`, `sit_login`, `sit_logout`, `status`) VALUES
+(62, 31313123, 'C# Programming', '524', '2024-03-18', '0000-00-00', 'Active'),
+(355, 19835644, 'C Programming', '524', '2024-03-18', '0000-00-00', 'Active'),
+(393, 19835644, 'C Programming', '524', '2024-03-18', '0000-00-00', 'Active');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id_number`),
+  ADD UNIQUE KEY `id_number` (`id_number`);
+
+--
+-- Indexes for table `student_session`
+--
+ALTER TABLE `student_session`
+  ADD PRIMARY KEY (`id_number`);
+
+--
+-- Indexes for table `student_sit_in`
+--
+ALTER TABLE `student_sit_in`
+  ADD PRIMARY KEY (`sit_id`),
+  ADD UNIQUE KEY `sit_id` (`sit_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
