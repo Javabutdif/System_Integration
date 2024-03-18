04bb00e348c7709e17e672db18aced8f57b8a4fc
unknown <TT128@ccs.local>
2024-03-19 03:32:05 +0800

    sit in update

--- static/Records.php
+++ static/Records.php
@@ -1,3 +1,4 @@
+
 <?php
   session_start();
   $con = mysqli_connect('localhost', 'root', '', 'ccs_system');
@@ -6,33 +7,101 @@
 		exit();
 	} 
 
-    if(isset($_GET["logout"])){
-        $id = $_GET["idNumber"];
-        $sql = "UPDATE `student_sit_in` SET `status` = 'Finished' WHERE `id_number` = '$id' ";
+  
+  class Student {
+    // Properties (attributes)
+    public  $id;
+    public  $name;
+    public  $records;
+
+    // Constructor method
+  
+    public function __construct($id, $name, $records) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->records = $records;
+    }
+   
+  }
+
+    if(isset($_GET["search"])) {
+      $search = $_GET["searchBar"];
+  
+   
+  
+
+  
+    // Prepare and bind the SQL statement
+    $sql = "SELECT * FROM students WHERE id_number = ? OR lastName = ? OR firstName = ?";
+    $stmt = $con->prepare($sql);
+    $stmt->bind_param("sss", $search, $search, $search);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if ($result->num_rows > 0 ) {
+        // Fetch the user data
+        $user = $result->fetch_assoc();
+       
+        // Fetch the session data
+        $sql1 = "SELECT * FROM student_session WHERE id_number = ?";
+        $stmt1 = $con->prepare($sql1);
+        $stmt1->bind_param("s", $user["id_number"]);
+        $stmt1->execute();
+        $result1 = $stmt1->get_result();
+        $record = $result1->fetch_assoc();
+        
+  
+        $student = new Student($user["id_number"], $user["firstName"]." ".$user["middleName"]." ".$user["lastName"], $record["session"]);
+        
+  
+        $displayModal = true;
+    } else {
+      
+      echo '  <script>
+     alert("No Student found!");
+        </script>';
+      
+      
+       
+    }
+  }
+  
+// get the post records
+if(isset($_POST["sitIn"])){
+
+  $sit_id = rand(1,1000);
+  $idNum = $_POST['studentID'];
+  $purpose = $_POST['purpose'];
+  $lab = $_POST['lab'];
+  $login = date('Y-m-d');
+  
+  
+  // database insert SQL code
+  
+  $sql = "INSERT INTO `student_sit_in` (`sit_id`,`id_number`, `sit_purpose`, `sit_lab`, `sit_login` , `status`)
+   VALUES ('$sit_id','$idNum', '$purpose', '$lab', '$login' , 'Active')";
+  
+  // insert in database 
+  if (mysqli_query($con, $sql)) {
+    echo '<script>window.alert("Student Sit-In Successful")</script>'; 
+    
+
+  }
+  else{
+
+    
+  }
+  
+  }
+
+
+
+
+
  
 
-// insert in database 
-if (mysqli_query($con, $sql) ) {
-	echo '<script>window.alert-success("Success!")</script>'; 
-	
+?>
 
-}
-else{
-	
-	echo '<script>alert("Error! Duplicate Id Number")</script>'; 
-	
-}
-
-
-    }
-
-       
-        
-        
-
-
-
-    ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -128,21 +197,122 @@ else{
                 <td><?php echo $person['session']; ?></td>
                 <td><?php echo $person['status']; ?></td>
 
-                <form action="Records.php" method="POST">
+            
                 <td class="d-inline-flex p-3 gap-2">
                   
-                <button type="button" name="logout"class="btn btn-danger">Log out</button> 
+                <button type="button" name="logout" onclick="logOut();"  class="btn btn-danger">Log out</button> 
                 <input type="hidden" name="idNumber" value="<?php echo $person['id_number']; ?>" />
                 
                  
                 </td>
-                </form>
+            
             </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
 
 </form>
+
+
+
+
+<!-- Modal -->
+<form action="Records.php" method="GET">
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-center " id="exampleModalLabel">Search Student</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center">
+        <input type="text" name="searchBar" placeholder="Search...">
+      </div>
+      <div class="modal-footer">
+        
+        <button type="submit" name="search"  class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+
+<!-- Vertical Modal-->
+
+<!-- Modal -->
+<form action="Records.php" method="POST">
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Sit In Form</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center container container-fluid">
+    <div class="form-group row">
+        <label for="id" class="col-sm-4 col-form-label">ID Number:</label>
+        <div class="col-sm-8">
+            <input id="id" name="studentID" type="text" value="<?php echo $student->id?>" readonly class="form-control"/>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="name" class="col-sm-4 col-form-label">Student Name:</label>
+        <div class="col-sm-8">
+            <input id="name" name="studentName" type="text" value="<?php echo  $student->name?>" readonly class="form-control"/>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="purposes" class="col-sm-4 col-form-label">Purpose:</label>
+        <div class="col-sm-8">
+            <select name="purpose" id="purposes" class="form-control">
+                <option value="C Programming">C Programming</option>
+                <option value="Java Programming">Java Programming</option>
+                <option value="C# Programming">C# Programming</option>
+                <option value="Php Programming">Php Programming</option>
+                <option value="ASP.Net Programming">ASP.Net Programming</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="lab" class="col-sm-4 col-form-label">Lab:</label>
+        <div class="col-sm-8">
+            <select name="lab" id="lab" class="form-control">
+                <option value="524">524</option>
+                <option value="526">526</option>
+                <option value="528">528</option>
+                <option value="530">530</option>
+                <option value="542">542</option>
+                <option value="Mac">Mac Laboratory</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="name" class="col-sm-4 col-form-label">Remaining Session: </label>
+        <div class="col-sm-8">
+            <input id="name" type="text" value="<?php echo  $student->records?>" readonly class="form-control"/>
+        </div>
+    </div>
+</div>
+
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" name="sitIn" class="btn btn-primary" >Sit In</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+
+
+
 
     
 
@@ -168,6 +338,34 @@ else{
   ?>
   new DataTable('#example');
 
+  function logOut(){
+    <?php
+      $con = mysqli_connect('localhost', 'root', '', 'ccs_system');
+          if(isset($_GET["logout"])){
+            $id = $_GET["idNumber"];
+            $sql = "UPDATE `student_sit_in` SET `status` = 'Finished' WHERE `id_number` = '$id' ";
+     
+    
+    // insert in database 
+    if (mysqli_query($con, $sql) ) {
+        echo '<script>window.alert-success("Success!")</script>'; 
+        
+    
+    }
+    else{
+        
+        echo '<script>alert("Error! Duplicate Id Number")</script>'; 
+        
+    }
+    
+    
+        }
+    
+        
+        
+        ?>
+  }
+
   
   
   
@@ -175,3 +373,15 @@ else{
 </script>
 </body>
 </html>
+
+
+<script>
+  <?php if ($displayModal): ?>
+            $(document).ready(function(){
+                $('#exampleModalCenter').modal('show');
+            });
+        <?php
+          
+      endif; ?>
+  </script>
+   
