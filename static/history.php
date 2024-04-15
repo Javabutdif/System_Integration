dab09f55f24e9898bfe6a5f30a69f26d6bf0f799
unknown <TT128@ccs.local>
2024-04-16 02:32:07 +0800

    update history

--- static/history.php
+++ static/history.php
@@ -38,7 +38,7 @@ $con = mysqli_connect('localhost', 'root', '', 'ccs_system');
     
 
 <div class="container">
-  
+  <a class="btn btn-danger" href="Homepage.php">Back</a>
   <table id="example" class="table table-striped display compact" style="width:100%">
   <thead style="background-color: #144c94">
       <tr>
@@ -77,6 +77,15 @@ $con = mysqli_connect('localhost', 'root', '', 'ccs_system');
   </tbody>
 </table>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.bootstrap5.js"></script>
+    
     
 </body>
 </html>
