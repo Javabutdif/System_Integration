a3f149a038cf67ac17190959c4c4489342f7ebf2
unknown <TT128@ccs.local>
2024-05-07 04:03:22 +0800

    search bar modal active but no function

--- Controller/api_admin.php
+++ Controller/api_admin.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
@@ -74,12 +75,122 @@
     </nav>
 
 
+
+<!-- Modal -->
+<form action="Admin.php" method="GET">
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-center " id="exampleModalLabel">Search Student</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center">
+        <input type="text" name="searchBar" placeholder="Search...">
+      </div>
+      <div class="modal-footer">
+        
+        <button type="submit" name="search"  class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+
+<!-- Vertical Modal-->
+
+<!-- Modal -->
+<form action="Admin.php" method="POST">
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Sit In Form</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center container container-fluid">
+    <div class="form-group row">
+        <label for="id" class="col-sm-4 col-form-label">ID Number:</label>
+        <div class="col-sm-8">
+            <input id="id" name="studentID" type="text" value="<?php echo $student->id?>" readonly class="form-control"/>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="name" class="col-sm-4 col-form-label">Student Name:</label>
+        <div class="col-sm-8">
+            <input id="name" name="studentName" type="text" value="<?php echo  $student->name?>" readonly class="form-control"/>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="purposes" class="col-sm-4 col-form-label">Purpose:</label>
+        <div class="col-sm-8">
+            <select name="purpose" id="purposes" class="form-control">
+                <option value="C-Programming">C Programming</option>
+                <option value="Java Programming">Java Programming</option>
+                <option value="C# Programming">C# Programming</option>
+                <option value="Php Programming">Php Programming</option>
+                <option value="ASP.Net Programming">ASP.Net Programming</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="lab" class="col-sm-4 col-form-label">Lab:</label>
+        <div class="col-sm-8">
+            <select name="lab" id="lab" class="form-control">
+                <option value="524">524</option>
+                <option value="526">526</option>
+                <option value="528">528</option>
+                <option value="530">530</option>
+                <option value="542">542</option>
+                <option value="Mac">Mac Laboratory</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="name" class="col-sm-4 col-form-label">Remaining Session: </label>
+        <div class="col-sm-8">
+            <input id="name" type="text" value="<?php echo  $student->records?>" readonly class="form-control"/>
+        </div>
+    </div>
+</div>
+
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" name="sitIn" class="btn btn-primary" >Sit In</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+
+
+
+
+
+
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
 <script src="https://cdn.datatables.net/2.0.6/js/dataTables.bootstrap5.js"></script>
 <script src="https://cdn.datatables.net/2.0.6/js/dataTables.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.bootstrap5.js"></script>
+    
 
 </body>
 </html>
